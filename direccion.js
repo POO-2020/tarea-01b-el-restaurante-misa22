dca43cf0d8c6454b87f2f01c06302b3b136be95a
misa22 <misael.solano.m@gmail.com>
2020-02-18 12:17:01 -0600

    class direccion

---
+++ direccion.js
@@ -0,0 +1,29 @@
+class Direccion {
+    /**
+     * @param {string} calle
+     * @param {number} numeroExteriro
+     * @param {number} numeroInterior
+     * @param {string} colonia
+     * @param {number} codigoPostal
+     * @param {string} ciudad 
+     * @param {string} municipio
+     */
+    constructor(calle, numeroExteriro, numeroInterior, colonia, codigoPostal, ciudad, municipio) {
+        this.calle = calle;
+        this.numeroExteriro = numeroExteriro;
+        this.numeroInterior = numeroInterior;
+        this.colonia = colonia;
+        this.codigoPostal = codigoPostal;
+        this.ciudad = ciudad;
+        this.municipio = municipio;
+    }
+    getFormatoCorto() {
+        return `${this.calle},${this.numeroExteriro}`;
+    }
+    getFormatoExtendido() {
+        return `${this.calle},${this.numeroExteriro},${this.numeroInterior},${this.colonia},${this.codigoPostal},${this.ciudad},${this.municipio}`
+    }
+}
+/*var Direccion1 = new Direccion("morelos",23,"A","conchita",28510,"colima","cuahutemoc");
+console.log(Direccion1.getFormatoCorto());
+console.log(Direccion1.getFormatoExtendido());*/
